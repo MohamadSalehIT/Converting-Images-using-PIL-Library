dbc43c75dedd92296ef82bf5f58a8e9bda0b37e0
MohamadSalehIT <123941794+MohamadSalehIT@users.noreply.github.com>
2023-03-23 14:02:24 +0200

    Update Rotate-Resize-Convert.py

--- Rotate-Resize-Convert.py
+++ Rotate-Resize-Convert.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 import os
-from PIL import Image
 
+from PIL import Image
+#this is the directory containing the images
 inputFolder  = 'images/'
-outputFolder = '/opt/icons'
+#this is the directory where the formatted images will be placed
+outputFolder = 'newImages'
 
 for image in os.listdir(inputFolder):
   if image != ".DS_Store":
         im = Image.open(os.path.join(inputFolder, image))
+      #Re-rotating the image to the right place
         im = im.rotate(-90)
+      #Resizing the image from 192x192 to 128x128
         im = im.resize((128,128))
         im = im.convert("RGB")
+      #Converting the images to the JPEG format and placing them in the new directory
         im.save(os.path.join(outputFolder, image+".jpeg"))
